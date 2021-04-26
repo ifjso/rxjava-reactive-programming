79d2c2b803ec5be28f7bc68e6203f0f06c0804ae
ifjso <ojsboy@hotmail.com>
2021-04-26 22:34:19 +0900

    feat: Flowable, Subscriber 기본 예제 추가

---
+++ src/main/java/com/js/rxjava/L11_FlowableSample.java
@@ -0,0 +1,59 @@
+package com.js.rxjava;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+public class L11_FlowableSample {
+
+    public static void main(String[] args) throws InterruptedException {
+        Flowable<String> flowable = Flowable.create(emitter -> {
+            String[] datas = {"Hello, World!", "안녕, RxJava!"};
+
+            for (String data : datas) {
+                if (emitter.isCancelled()) {
+                    return;
+                }
+
+                emitter.onNext(data);
+            }
+
+            emitter.onComplete();
+        }, BackpressureStrategy.BUFFER);
+
+        flowable
+                .observeOn(Schedulers.computation())
+                .subscribe(new Subscriber<>() {
+                    private Subscription subscription;
+
+                    @Override
+                    public void onSubscribe(Subscription subscription) {
+                        this.subscription = subscription;
+                        this.subscription.request(1L);
+                    }
+
+                    @Override
+                    public void onNext(String data) {
+                        String threadName = Thread.currentThread().getName();
+                        System.out.println(threadName + ": " + data);
+                        this.subscription.request(1L);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        String threadName = Thread.currentThread().getName();
+                        System.out.println(threadName + ": 완료");
+                    }
+
+                    @Override
+                    public void onError(Throwable error) {
+                        error.printStackTrace();
+                    }
+                });
+
+        Thread.sleep(500L);
+
+    }
+}
