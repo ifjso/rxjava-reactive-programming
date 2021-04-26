28018de01c8496197692279c3c4c0b2f85c52704
ifjso <ojsboy@hotmail.com>
2021-04-26 22:45:23 +0900

    feat: Observable, Observer 기본 예제 추가

---
+++ src/main/java/com/js/rxjava/L18_ObservableSample.java
@@ -0,0 +1,53 @@
+package com.js.rxjava;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class L18_ObservableSample {
+    public static void main(String[] args) throws InterruptedException {
+        Observable<String> observable = Observable.create(emitter -> {
+            String[] datas = {"Hello, World!", "안녕, RxJava!"};
+
+            for (String data : datas) {
+                if (emitter.isDisposed()) {
+                    return;
+                }
+
+                emitter.onNext(data);
+            }
+
+            emitter.onComplete();
+        });
+
+        observable
+                .observeOn(Schedulers.computation())
+                .subscribe(new Observer<>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable disposable) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull String item) {
+                        String threadName = Thread.currentThread().getName();
+                        System.out.println(threadName + ": " + item);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        String threadName = Thread.currentThread().getName();
+                        System.out.println(threadName + ": 완료");
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        e.printStackTrace();
+                    }
+                });
+
+        Thread.sleep(500L);
+    }
+}
