ce898be5be7d5e4b3fe23c0be8a1634f83ced231
ifjso <ojsboy@hotmail.com>
2021-04-27 05:40:39 +0900

    feat: Completable 예제 추가

---
+++ src/main/java/com/js/rxjava/L32_CompletableSample.java
@@ -0,0 +1,36 @@
+package com.js.rxjava;
+
+import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class L32_CompletableSample {
+    public static void main(String[] args) throws InterruptedException {
+        Completable completable = Completable.create(emitter -> {
+            emitter.onComplete();
+        });
+
+        completable
+                .subscribeOn(Schedulers.computation())
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("완료");
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        System.out.println("에러=" + e);
+                    }
+                });
+
+        Thread.sleep(100L);
+    }
+}
