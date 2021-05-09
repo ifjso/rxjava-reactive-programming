0c4bf8123ce14a0f451a58a59202380adeaab36d
ifjso <ojsboy@hotmail.com>
2021-05-09 17:13:09 +0900

    feat: just 예제 추가

---
+++ src/main/java/com/js/rxjava/chapter4/JustSample.java
@@ -0,0 +1,11 @@
+package com.js.rxjava.chapter4;
+
+import io.reactivex.Flowable;
+
+public class JustSample {
+
+    public static void main(String[] args) {
+        Flowable<String> flowable = Flowable.just("A", "B", "C", "D", "E");
+        flowable.subscribe(new DebugSubscriber<>());
+    }
+}
