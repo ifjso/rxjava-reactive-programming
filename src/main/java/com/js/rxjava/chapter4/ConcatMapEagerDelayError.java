a894f7ee21aed0be4e1eb19853318ab04c5eeb1f
ifjso <ojsboy@hotmail.com>
2021-05-10 10:25:06 +0900

    feat: concatMapEagerDelayError 예제 추가

---
+++ src/main/java/com/js/rxjava/chapter4/ConcatMapEagerDelayError.java
@@ -0,0 +1,25 @@
+package com.js.rxjava.chapter4;
+
+import io.reactivex.Flowable;
+import java.util.concurrent.TimeUnit;
+
+public class ConcatMapEagerDelayError {
+
+    public static void main(String[] args) throws InterruptedException {
+        Flowable<String> flowable = Flowable.range(10, 3)
+            .concatMapEagerDelayError(
+                sourceData -> Flowable.interval(500L, TimeUnit.MILLISECONDS)
+                    .take(3)
+                    .doOnNext(data -> {
+                        if (sourceData == 11 && data == 1) {
+                            throw new Exception("예외 발생");
+                        }
+                    })
+                    .map(data -> "[" + sourceData + "] " + data),
+                true);
+
+        flowable.subscribe(new DebugSubscriber<>());
+
+        Thread.sleep(4000L);
+    }
+}
