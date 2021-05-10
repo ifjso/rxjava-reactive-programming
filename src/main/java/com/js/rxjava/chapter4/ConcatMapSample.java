19417df5c07f4de458024a10949e46feb6e5656e
ifjso <ojsboy@hotmail.com>
2021-05-10 10:09:49 +0900

    feat: concatMap, cancatMapEager 예제 추가

---
+++ src/main/java/com/js/rxjava/chapter4/ConcatMapSample.java
@@ -0,0 +1,23 @@
+package com.js.rxjava.chapter4;
+
+import io.reactivex.Flowable;
+
+import java.util.concurrent.TimeUnit;
+
+public class ConcatMapSample {
+
+    public static void main(String[] args) throws InterruptedException {
+        Flowable<String> flowable = Flowable.range(10, 3)
+                .concatMap(
+                        sourceData -> Flowable.interval(500L, TimeUnit.MILLISECONDS)
+                                .take(2)
+                                .map(data -> {
+                                    long time = System.currentTimeMillis();
+                                    return time + "ms: [" + sourceData + "] " + data;
+                                }));
+
+        flowable.subscribe(new DebugSubscriber<>());
+
+        Thread.sleep(4000L);
+    }
+}
