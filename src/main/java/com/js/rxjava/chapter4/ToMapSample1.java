beef44bbeabaaa68c67e9786e79bb64a8c338944
ifjso <ojsboy@hotmail.com>
2021-05-11 06:37:49 +0900

    feat: toList, toMap, toMultimap 예제 추가

---
+++ src/main/java/com/js/rxjava/chapter4/ToMapSample1.java
@@ -0,0 +1,16 @@
+package com.js.rxjava.chapter4;
+
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+
+import java.util.Map;
+
+public class ToMapSample1 {
+
+    public static void main(String[] args) {
+        Single<Map<Long, String>> single = Flowable.just("1A", "2B", "3C", "1D", "2E")
+                .toMap(data -> Long.valueOf(data.substring(0, 1)));
+
+        single.subscribe(new DebugSingleObserver<>());
+    }
+}
---
+++ src/main/java/com/js/rxjava/chapter4/ToMapSample2.java
@@ -0,0 +1,18 @@
+package com.js.rxjava.chapter4;
+
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+
+import java.util.Map;
+
+public class ToMapSample2 {
+
+    public static void main(String[] args) {
+        Single<Map<Long, String>> single = Flowable.just("1A", "2B", "3C", "1D", "2E")
+                .toMap(
+                        data -> Long.valueOf(data.substring(0, 1)),
+                        data -> data.substring(1));
+
+        single.subscribe(new DebugSingleObserver<>());
+    }
+}
