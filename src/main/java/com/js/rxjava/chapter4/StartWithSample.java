29b02415aabbe1563a0e4d208f16cf102e91121d
ifjso <ojsboy@hotmail.com>
2021-05-18 00:36:38 +0900

    feat: startWith, zip, combine 예제 추가

---
+++ src/main/java/com/js/rxjava/chapter4/StartWithSample.java
@@ -0,0 +1,23 @@
+package com.js.rxjava.chapter4;
+
+import io.reactivex.Flowable;
+
+import java.util.concurrent.TimeUnit;
+
+public class StartWithSample {
+
+    public static void main(String[] args) throws InterruptedException {
+        Flowable<Long> flowable = Flowable.interval(300L, TimeUnit.MILLISECONDS)
+                .take(5);
+
+        Flowable<Long> other = Flowable.interval(500L, TimeUnit.MILLISECONDS)
+                .take(2)
+                .map(data -> data + 100L);
+
+        Flowable<Long> result = flowable.startWith(other);
+
+        result.subscribe(new DebugSubscriber<>());
+
+        Thread.sleep(3000L);
+    }
+}
