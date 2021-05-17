9a31d3889ee8d84e543d330441e5c5548f4c571b
ifjso <ojsboy@hotmail.com>
2021-05-18 03:07:19 +0900

    feat: timeout 예제 추가

---
+++ src/main/java/com/js/rxjava/chapter4/TimeoutSample.java
@@ -0,0 +1,31 @@
+package com.js.rxjava.chapter4;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+
+import java.util.concurrent.TimeUnit;
+
+public class TimeoutSample {
+
+    public static void main(String[] args) throws InterruptedException {
+        Flowable<Integer> flowable = Flowable.<Integer>create(emitter -> {
+            emitter.onNext(1);
+            emitter.onNext(2);
+
+            try {
+                Thread.sleep(1200L);
+            } catch (InterruptedException e) {
+                emitter.onError(e);
+                return;
+            }
+
+            emitter.onNext(3);
+            emitter.onComplete();
+        }, BackpressureStrategy.BUFFER)
+                .timeout(1000L, TimeUnit.MILLISECONDS);
+
+        flowable.subscribe(new DebugSubscriber<>());
+
+        Thread.sleep(2000L);
+    }
+}
